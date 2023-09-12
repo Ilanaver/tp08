cdcb65f077fa59489dd160aead470910a2d4233d
FRANCO PASQUALE SANGUINETTI <47573419@ort>
2023-09-12 09:02:51 -0300

    v1

---
+++ js/ejercicios.js
@@ -0,0 +1,18 @@
+const frutas = ["manzana", "pera", "durazno", "kiwi", "pomelo", "sandia", "uva", "banana", "ciruela", "melon"] ;
+
+
+console.log(frutas);
+let EleccionFruta = prompt("¿Que fruta desea?")
+if(EleccionFruta != frutas.fruta)
+alert("No, no tenemos" + EleccionFruta ,"!");
+if(EleccionFruta != frutas.fruta)
+alert("Sí, tenemos" + EleccionFruta + "!");
+
+
+const Ciudad = {
+    nombre: "",
+    FechaFundacion:12/02/1987 ,
+    poblacion:10 ,
+    extension: 1000000
+
+}
