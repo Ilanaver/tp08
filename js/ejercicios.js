b4ee7f9cca6e5d01527277dd50dbebc86bde05e4
ILÁN AVERBUCH <47756465@ort>
2023-09-12 09:03:29 -0300

    Agrego la 1

---
+++ js/ejercicios.js
@@ -0,0 +1,34 @@
+/*Calculadora de edad
+
+Crear una función que reciba un string con una fecha en formato 'YYYY-MM-DD' y devuelva un número con la edad de la persona.
+
+Pedir al usuario su nombre y su fecha de nacimiento. Luego, mostrar en la consola un mensaje que diga "Hola [nombre], tienes [edad] años!"
+*/
+function calcularEdad() {
+    const nombre = prompt('Por favor, ingresa tu nombre:');
+    const fechaNacimiento = prompt('Por favor, ingresa tu fecha de nacimiento en formato YYYY-MM-DD:');
+    var fechaNacimientoDate = new Date(fechaNacimiento);
+    var fechaActual = new Date();
+    const edad = fechaActual.getFullYear() - fechaNacimientoDate.getFullYear();
+    
+    // Ajustar la edad si el cumpleaños aún no ha ocurrido este año
+    if (
+      fechaNacimientoDate.getMonth() > fechaActual.getMonth() ||
+      (fechaNacimientoDate.getMonth() === fechaActual.getMonth() &&
+        fechaNacimientoDate.getDate() > fechaActual.getDate())
+    ) {
+      edad--;
+    }
+    console.log('Hola ' + nombre + ', tienes ' + edad + ' años!');
+
+}
+  
+  // Solicitar el nombre y la fecha de nacimiento al usuario
+
+  
+  // Calcular la edad del usuario utilizando la función calcularEdad
+  const edad = calcularEdad();
+  
+  // Mostrar el mensaje con la edad calculada
+
+  
