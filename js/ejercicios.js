d34bc30ea0a1c6c9ac5fa85cd8f72133feebedfb
ILÁN AVERBUCH <47756465@ort>
2023-09-14 15:50:28 -0300

    Termine la 2

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -2,8 +2,8 @@
 
 Crear una función que reciba un string con una fecha en formato 'YYYY-MM-DD' y devuelva un número con la edad de la persona.
 
-Pedir al usuario su nombre y su fecha de nacimiento. Luego, mostrar en la consola un mensaje que diga "Hola [nombre], tienes [edad] años!"
-*/
+Pedir al usuario su nombre y su fecha de nacimiento. Luego, mostrar en la consola un mensaje que diga "Hola [nombre], tienes [edad] años!"*/
+
 function calcularEdad(fechaNacimiento) {
   var fechaNacimientoDate = new Date(fechaNacimiento);
   var fechaActual = new Date();
@@ -15,16 +15,28 @@ function calcularEdad(fechaNacimiento) {
   return edad;
 }
 
-/*
+/*Crear un array de strings con 10 frutas y mostrarlo en consola de manera prolija.
+
+Pedir al usuario una fruta. Buscar la fruta en el array. Si la fruta existe,
+
+mostrar un mensaje que diga "Sí, tenemos [fruta]!”. Si no existe, mostrar un mensaje que diga "No, no tenemos [fruta]!"*/
+
 const frutas = ["manzana", "pera", "durazno", "kiwi", "pomelo", "sandia", "uva", "banana", "ciruela", "melon"] ;
-
-
 console.log(frutas);
-let EleccionFruta = prompt("¿Que fruta desea?")
-if(EleccionFruta != frutas.fruta)
-alert("No, no tenemos" + EleccionFruta ,"!");
-if(EleccionFruta != frutas.fruta)
-alert("Sí, tenemos" + EleccionFruta + "!");
+function Existfrutas(EleccionFruta){
+  var frutaEncontrada = false;
+  for (var i = 0; i < frutas.length; i++) {
+      if (frutas[i].toLowerCase() === EleccionFruta.toLowerCase()) {
+          frutaEncontrada = true;
+          break;
+      }
+  }
+  if (frutaEncontrada) {
+    console.log("Sí, tenemos " + EleccionFruta + "!");
+  } else {
+    console.log("No, no tenemos " + EleccionFruta + "!");
+  }  
+}
 
 
 const Ciudad = {
@@ -34,4 +46,4 @@ const Ciudad = {
     extension: 1000000
 
 }
-*/
+
--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,12 @@
 //1
+console.log("ejercicio 1");
 const nombre = prompt('Por favor, ingresa tu nombre:');
 const fechaNacimiento = prompt('Por favor, ingresa tu fecha de nacimiento en formato YYYY-MM-DD:');
 
 const edad = calcularEdad(fechaNacimiento);
 console.log('Hola ' + nombre + ', tienes ' + edad + ' años!');
+
+//2
+console.log("ejercicio 2");
+let EleccionFruta = prompt("¿Que fruta desea?")
+const fruta = Existfrutas(EleccionFruta);
